a647da57c5ff0f01e3663af5555f775f30d3ea22
Obooky <obooky@Z-COM.localdomain>
2026-08-19 01:38:20 +0800

    update

--- src/macro/arbchain.v
+++ src/macro/arbchain.v
@@ -1,20 +1,12 @@
 `timescale 1ns/1ps
 // behavioural model of the arbchain macro (RTL simulation only)
-module arbchain (
-  q, launch, arb_rst_n, ch
-);
-  output q;
-  input launch;
-  input arb_rst_n;
-  input [23:0] ch;
-
+`ifdef USE_POWER_PINS
+module arbchain (q, launch, arb_rst_n, ch, VPWR, VGND);
+`else
+module arbchain (q, launch, arb_rst_n, ch);
+`endif
+  output q; input launch; input arb_rst_n; input [23:0] ch;
   wire top_out, bot_out;
-
-  arbiter_chain #(.STAGES(24), .IDX(0)) u_chain (
-    .launch(launch), .ch(ch), .top_out(top_out), .bot_out(bot_out)
-  );
-
-  arbiter_cell u_arbiter (
-    .top_in(top_out), .bot_in(bot_out), .arb_rst_n(arb_rst_n), .q(q)
-  );
+  arbiter_chain #(.STAGES(24), .IDX(0)) u_chain (.launch(launch), .ch(ch), .top_out(top_out), .bot_out(bot_out));
+  arbiter_cell u_arbiter (.top_in(top_out), .bot_in(bot_out), .arb_rst_n(arb_rst_n), .q(q));
 endmodule
--- src/macro/arbchain.vh
+++ src/macro/arbchain.vh
@@ -1,21 +1,14 @@
 `ifdef USE_POWER_PINS
 `celldefine
 module arbchain (
-  output q,
-  input launch,
-  input arb_rst_n,
-  input [23:0] ch,
-  input VPWR,
-  input VGND
+  output q, input launch, input arb_rst_n, input [23:0] ch,
+  input VPWR, input VGND
 );
 endmodule
 `endcelldefine
 `else
 module arbchain (
-  output q,
-  input launch,
-  input arb_rst_n,
-  input [23:0] ch
+  output q, input launch, input arb_rst_n, input [23:0] ch
 );
 endmodule
 `endif
